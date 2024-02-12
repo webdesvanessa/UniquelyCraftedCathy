c8c0d639101e5999d26bab6321e218e273e03a4c
Vanessa <vlombard1998@gmail.com>
2024-02-12 15:37:11 +0200

    gh-pages cleanup

--- UniquelyCrafted/schemas/index.js
+++
@@ -1,7 +0,0 @@
-import productListing from "./product-listing"
-import category from "./category"
-import colour from "./colour"
-import twoTone from "./two-tone"
-import pattern from "./pattern"
-
-export const schemaTypes = [productListing, category, colour, twoTone, pattern]
--- docs/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{c as a}from"./chunk-PKXM2HNV.mjs";import"./chunk-B2YMF6CZ.mjs";import"./chunk-YELSAYKL.mjs";export{a as default};
