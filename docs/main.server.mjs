c013f072286f666c2d215762435685ab2b0595f1
Vanessa <vlombard1998@gmail.com>
2024-02-12 17:10:43 +0200

    Rebuild 2

--- docs/main.server.mjs
+++ docs/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{c as a}from"./chunk-5MF5AKVP.mjs";import"./chunk-4VUQZY6H.mjs";import"./chunk-YELSAYKL.mjs";export{a as default};
+import{c as a}from"./chunk-T5XHJMO2.mjs";import"./chunk-4VUQZY6H.mjs";import"./chunk-YELSAYKL.mjs";export{a as default};
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { SanityService } from '../sanity.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
+  // FETCH DATA FROM SANITY CMS:
+
+  logo: any;
+
+  logo_query = '*[_type == "site_images"]{"img": logo.asset->url}'
+
+  constructor(private sanityService: SanityService) {}
+
+  ngOnInit(): void {
+    this.sanityService.getClient().fetch(this.logo_query).then((data) => {
+      this.logo = data[0]["img"];
+      console.log(this.logo);
+    });
+  }
 }
